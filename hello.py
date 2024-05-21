9c26d66817c890099ab3070d46cdd8a38217c5eb
22judson <majudson@colostate.edu>
2024-05-21 14:07:57 +0200

    hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 def hello_world():
 	return "Hello World!"
 def hello_world_n(N):
+	Hello_list = []
 	while N != 0:
 		N -= 1
-		print("Hello World! ", end='')
+		Hello_list.append("Hello World!)
+	return Hello_list
